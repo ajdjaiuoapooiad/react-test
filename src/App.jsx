86d502bba065ba0ed80c31c15430f11ea4472cd3
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-17 19:52:59 +0900

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
+import data from "./data";
 
 
 const App = () => {
+  const [ posts,setPosts ] = useState(data)
+  const categorys = Array.from(new Set(posts.map((post) => post.category)));
+  const incomes = Array.from(new Set(posts.map((post) => post.income)))
+
+  const selectCategory = (category) => {
+    if(category==='all'){
+      setPosts(posts)
+      return;
+    }
+
+
+    const selectedPosts = posts.filter((post) => post.category === category)
+    setPosts(selectedPosts);
+  }
+
+  const selectIncome = (income) => {
+
+    const selectedPosts = posts.filter((post) => post.income === income)
+    setPosts(selectedPosts);
+  }
+
+
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark p-3">
       <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -12,7 +36,7 @@ const App = () => {
       <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
         <ul className="navbar-nav mr-auto mt-2 mt-md-0">
           <li className="nav-item active">
-            <a className="nav-link  text-white" href="#!">Search </a>
+            <a className="nav-link  text-white " href="#!">Search </a>
           </li>
           <li className="nav-item">
             <a className="nav-link  text-white" href="#!">Create</a>
@@ -21,18 +45,46 @@ const App = () => {
       </div>
     </nav>
 
-      <div className="w-25 m-5 border border-dark">
+      <div className="w-25">
 
-        <button className="btn btn-info">test</button><br />
-        <button className="btn btn-info">test</button>
-        <button className="btn btn-info">test</button>
-        <button className="btn btn-info">test</button>
-        <button className="btn btn-info">test</button>
-        <button className="btn btn-info">test</button>
+      <ul>
+        <button onClick={() => selectCategory('all')}>all</button>
+        {categorys.map((c) => {
+          return (
+            <button onClick={() => selectCategory(c)} key={c.id} >{c}</button>
+          )
+        })}
+        
+      </ul>
+      {
+        incomes.map((income) => {
+          return(
+            <select className="w-40"  onChange={() => selectIncome(income)} name="item">
+            <option >{income}</option>
+            </select>
+          )
+        })
+      }
 
       </div>
 
-      <div className="w-75 m-5 border border-dark">
+      <div className="w-75">
+      <h2>求人一覧</h2>
+        <ul>
+
+        {posts.map((p) => {
+          return (
+            <div>
+              <li>{p.id}</li>
+              <li>{p.title}</li>
+              <li>{p.category}</li>
+              <li>{p.income}</li>
+            </div>
+          )
+        })}
+
+        </ul>
+
 
       </div>
 
---
+++ src/data.js
@@ -0,0 +1,30 @@
+
+
+const data = [
+    {
+        id: 1,
+        title: 'Test1',
+        income: 300,
+        category: 'Python',
+    },
+    {
+        id: 2,
+        title: 'Test2',
+        income: 400,
+        category: 'Django',
+    },
+    {
+        id: 3,
+        title: 'Test3',
+        income: 500,
+        category: 'React',
+    },
+    {
+        id: 4,
+        title: 'Test4',
+        income: 800,
+        category: 'Next.js',
+    },
+    ]
+    
+    export default data
