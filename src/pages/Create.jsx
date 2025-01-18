c61edca8a60efa2d921d9f99c7142e6d754952ef
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-18 14:13:40 +0900

    error ok

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,6 +1,15 @@
+import { useRef, useState } from "react"
+import data from '../data';
 
 
 const Create = () => {
+    const [ jobs,setJobs ] = useState(data)
+    const categorys = Array.from(new Set(jobs.map((j) => j.category)));
+    const jobTitleRef = useRef()
+    const jobIncomeRef = useRef()
+
+    
+
     return (
       <>
       <div className="p-5 ">
@@ -8,20 +17,28 @@ const Create = () => {
      
         <h1>Createページ</h1>
 
+        <form action="" >
+ 
         <h4>Title</h4>
-        <input type="text" />
+
+        <input type="text" ref={jobTitleRef} />
         <h4>Income</h4>
-        <input type="text" />
+        <input type="text" ref={jobIncomeRef} />
 
         <h4>Category</h4>
-        <select name="" id="">
-          <option value="">test</option>
-          <option value="">tets</option>
-          <option value="">test</option>
+        <select name="" id="" >
+          {categorys.map((c) => {
+            return (
+              <option value="">{c}</option>
+            )
+          })}
+        
+          
         </select>
 
-        <button className="btn btn-info">送信</button>
+        <button className="btn btn-info" type="submit">送信</button>
 
+        </form>
       </div>
       </>
     )
