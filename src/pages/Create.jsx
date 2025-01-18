715b043714a0c29da4bbcae1b6df566efae0170b
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-18 13:37:58 +0900

    Create ok

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -2,7 +2,28 @@
 
 const Create = () => {
     return (
-      <div>Create</div>
+      <>
+      <div className="p-5 ">
+
+     
+        <h1>Createページ</h1>
+
+        <h4>Title</h4>
+        <input type="text" />
+        <h4>Income</h4>
+        <input type="text" />
+
+        <h4>Category</h4>
+        <select name="" id="">
+          <option value="">test</option>
+          <option value="">tets</option>
+          <option value="">test</option>
+        </select>
+
+        <button className="btn btn-info">送信</button>
+
+      </div>
+      </>
     )
   }
   
