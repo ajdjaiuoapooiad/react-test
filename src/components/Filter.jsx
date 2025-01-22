eee5bd7ff22a8603da90a437fc8be6ed9d1f0e4a
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-22 15:20:21 +0900

    Api error Change

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -11,14 +11,7 @@ const Filter = () => {
   const { search, income, category } = params;
   return (
     <Form className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
-      {/* SEARCH */}
-      <FormInput
-        type='search'
-        label='search product'
-        name='search'
-        defaultValue={search}
-        size='input-sm'
-      />
+      
       {/* CATEGORIES */}
       <FormSelect
         label='select category'
