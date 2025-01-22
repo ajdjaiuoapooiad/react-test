bbe90c7ba8f4420dd95cc73c37409333faefb0d8
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-22 10:10:03 +0900

    db.json

--- src/data.js
+++ src/data.js
@@ -1,33 +1,112 @@
+{
+    "data": [
+        {
+            "id": 1,
+            "attributes": {
+            "title": "大企業内定多数！注目の松尾研発スタートアップで学生エンジニア採用強化！",
+            "category": "Python",
+            "income": 400,
+            }
+        },
+        {
+            "id": 10,
+            "attributes": {
+            "title": "avant-garde lamp",
+            "category": "Django",
+            "income": 500,
+            }
+        },
+        {
+            "id": 2,
+            "attributes": {
+            "title": "成長スタートアップでのAIエンジニア募集！学生からエンジニアの経験を！",
+            "category": "Django",
+            "income": 400,
+            }
+        },
+        {
+            "id": 3,
+            "attributes": {
+            "title": "リアルとデジタルの融合を目指すアプリケーションエンジニアを募集！",
+            "category": "React",
+            "income": 800,
+            }
+        },
+        {
+            "id": 4,
+            "attributes": {
+            "title": "京都でWebサービスの開発に関わりたいインターン生募集！",
+            "category": "React",
+            "income": 400,
+            }
+        },
+        {
+            "id": 5,
+            "attributes": {
+            "title": "大企業内定多数！注目の松尾研発スタートアップで学生エンジニア採用強化！",
+            "category": "Next.js",
+            "income": 300,
+            }
+        },
+        {
+            "id": 6,
+            "attributes": {
+            "title": "大企業内定多数！注目の松尾研発スタートアップで学生エンジニア採用強化！",
+            "category": "Next.js",
+            "income": 300,
+            }
+        },
+        {
+            "id": 7,
+            "attributes": {
+            "title": "大企業内定多数！注目の松尾研発スタートアップで学生エンジニア採用強化！",
+            "category": "Next.js",
+            "income": 300,
+            }
+        },
+        {
+            "id": 8,
+            "attributes": {
+            "title": "大企業内定多数！注目の松尾研発スタートアップで学生エンジニア採用強化！",
+            "category": "Next.js",
+            "income": 300,
+            }
+        },
+        {
+            "id": 9,
+            "attributes": {
+            "title": "大企業内定多数！注目の松尾研発スタートアップで学生エンジニア採用強化！",
+            "category": "Next.js",
+            "income": 300,
+            }
+        },
+        {
+            "id": 11,
+            "attributes": {
+            "title": "大企業内定多数！注目の松尾研発スタートアップで学生エンジニア採用強化！",
+            "category": "Next.js",
+            "income": 300,
+            }
+        },
+    ],
+    "meta": {
+        "categories": [
+            "all",
+            "Python",
+            "Django",
+            "React",
+            "Next",
+        ],
+        "incomes": [
+            200,
+            300,
+            400,
+            500,
+            600,
+            700,
+            800,
+            900,
+        ],
+    }
+}
 
-
-export default [
-    {
-        id: 1,
-        title: '大企業内定多数！注目の松尾研発スタートアップで学生エンジニア採用強化！',
-        income: '３００万',
-        category: 'Python',
-        featured: true,
-    },
-    {
-        id: 2,
-        title: '成長スタートアップでのAIエンジニア募集！学生からエンジニアの経験を！',
-        income: '４００万',
-        category: 'Django',
-        featured: true,
-    },
-    {
-        id: 3,
-        title: 'リアルとデジタルの融合を目指すアプリケーションエンジニアを募集！',
-        income: '５００万',
-        category: 'React',
-        featured: true,
-    },
-    {
-        id: 4,
-        title: '京都でWebサービスの開発に関わりたいインターン生募集！',
-        income: '８００万',
-        category: 'Next.js',
-        featured: false,
-    },
-    ]
-    
